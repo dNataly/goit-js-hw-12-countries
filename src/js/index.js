0f22eaa4293a3e5865f804bf8898228593bd667c
natalia_denysenko <nnmtshh@gmail.com>
2021-07-02 00:29:26 +0300

    changed debounce time

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ var debounce = require('lodash.debounce');
 
 fetchCountries(name);
 
-refs.input.addEventListener('input', debounce(getInputValue, 100));
+refs.input.addEventListener('input', debounce(getInputValue, 500));
 // refs.input.addEventListener('input', getInputValue);
 function getInputValue(e) {
   let inputValue = e.target.value.trim();
