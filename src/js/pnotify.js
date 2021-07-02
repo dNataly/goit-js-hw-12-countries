a8c5ec1fa3cfd0685cf78dfca2c1e8fe8172ad7e
natalia_denysenko <nnmtshh@gmail.com>
2021-07-02 17:47:02 +0300

    checking pnotify

--- src/js/pnotify.js
+++ src/js/pnotify.js
@@ -1,8 +1,8 @@
-// import { alert, defaultModules } from './node_modules/@pnotify/core/dist/PNotify.js';
-// import * as PNotifyMobile from './node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 
-// defaultModules.set(PNotifyMobile, {});
+defaultModules.set(PNotifyMobile, {});
 
-// alert({
-//   text: 'Notice me, senpai!',
-// });
+alert({
+  text: 'Notice me, senpai!',
+});
